a5762d4e0d1be11cdafbecf2a11936715c0db9f2
Crxssroad <gaby_encar28@hotmail.com>
2020-02-09 14:28:11 -0500

    Added tests for username validation.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_valid(:username).when("JaneDoe") }
+  it { should_not have_valid(:username).when(nil, "", "jane@doe.com") }
 end
