0098fd417e6b649df65389c9be061463da9ee9ac
Crxssroad <gaby_encar28@hotmail.com>
2020-02-10 17:41:06 -0500

    Added create method to journals_controller

--- app/controllers/api/v1/journals_controller.rb
+++ app/controllers/api/v1/journals_controller.rb
@@ -1,5 +1,23 @@
 class Api::V1::JournalsController < ApplicationController
+
   def index
     render json: current_user.journals
+  end
+
+  def create
+    journal = Journal.new(journal_params)
+    journal.user = current_user
+
+    if journal.save
+      render json: journal
+    else
+      render json: journal.errors.full_messages
+    end
+  end
+
+  private
+
+  def journal_params
+    params.require(:journal).permit(:title, :description)
   end
 end
