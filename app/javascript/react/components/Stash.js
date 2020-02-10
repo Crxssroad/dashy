967e35bbe6db2566185cbc01c0e13e9e8493e28e
Crxssroad <gaby_encar28@hotmail.com>
2020-02-10 14:11:20 -0500

    Created stash component and controller

--- app/javascript/react/components/Stash.js
+++ app/javascript/react/components/Stash.js
@@ -1,6 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Stash = () => {
+  const [journals, setJournals] = useState([])
+
+  useEffect(() => {
+    fetch('/api/v1/stashes')
+    .then(response => {
+      if (response.ok) {
+        return response.json()
+      } else {
+        throw new Error(`${response.status} ${response.statusText}`)
+      }
+    })
+    .then(parsedBody => {
+      setJournals(parsedBody.journals)
+    })
+    .catch(error => console.error(`Error in stash fetch ${error.message}`))
+  }, [])
+
   return(
     <h1>Stash</h1>
   )
