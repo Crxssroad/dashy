c0d4ffe44f21cc554124d12628b2997d8d0cf550
Crxssroad <gaby_encar28@hotmail.com>
2020-02-10 21:39:21 -0500

    Added destroy route for journal api.

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   namespace 'api' do
     namespace 'v1' do
       resources :stashes, only: [:index]
-      resources :journals, only: [:index, :show, :create, :update]
+      resources :journals, only: [:index, :show, :create, :update, :destroy]
     end
   end
 end
