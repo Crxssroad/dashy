3922712a76897949c9d41f5d33961385e93c2582
Crxssroad <gaby_encar28@hotmail.com>
2020-02-12 14:41:53 -0500

    Created profile_photo uploader and configured carrierwave with aws

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,10 @@ class User < ApplicationRecord
   attr_writer :login
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  mount_uploader :profile_photo, ProfilePhotoUploader
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
 
   has_many :journals
 
