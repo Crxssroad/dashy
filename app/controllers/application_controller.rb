15e359ec1d5298a2e756b21a23bc433b05735dea
Crxssroad <gaby_encar28@hotmail.com>
2020-02-12 14:42:42 -0500

    Modified devise and registrations controller to accept profile_photo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,9 +13,10 @@ class ApplicationController < ActionController::Base
       :email,
       :password,
       :password_confirmation,
-      :remember_me
+      :remember_me,
+      :profile_photo
     ]
-    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :signup, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
   end
 
