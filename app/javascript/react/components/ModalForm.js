1fae1f1903cb3701d49b292ae4fbb8a9713eba1d
Crxssroad <gaby_encar28@hotmail.com>
2020-02-13 14:16:49 -0500

    Added bootstrap modal

---
+++ app/javascript/react/components/ModalForm.js
@@ -0,0 +1,56 @@
+import React, { Fragment } from 'react'
+
+const ModalForm = (props) => {
+
+  let id, title, footer
+  if (props.type === "signup") {
+    id = "signupModal"
+    title = "Sign Up"
+    footer = <Fragment>
+      <span>Already a member? </span>
+      <a
+        href="#loginModal"
+        data-toggle="modal"
+        onClick={() => {$('#signupModal').modal('hide')}}
+      >
+        Log In
+      </a>
+    </Fragment>
+  }
+  if (props.type === "login") {
+    id = "loginModal"
+    title = "Log In"
+    footer = <Fragment>
+      <span>New User? </span>
+      <a
+        href="#signupModal"
+        data-toggle="modal"
+        onClick={() => {$('#loginModal').modal('hide')}}
+      >
+        Sign Up
+      </a>
+    </Fragment>
+  }
+  return (
+    <Fragment>
+      <div id={id} className="modal fade">
+      	<div className="modal-dialog modal-login">
+      		<div className="modal-content">
+      			<div className="modal-header">
+      				<h4 className="modal-title">{title}</h4>
+              <button type="button" className="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+      			</div>
+      			<div className="modal-body">
+      				{props.children}
+      			</div>
+      			<div className="modal-footer">
+      				{footer}
+      			</div>
+      		</div>
+      	</div>
+      </div>
+    </Fragment>
+  )
+}
+
+export default ModalForm;
