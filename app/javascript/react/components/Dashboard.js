7c659674211ad9283801f05d074a13e858b87df7
Crxssroad <gaby_encar28@hotmail.com>
2020-02-12 22:56:20 -0500

    Added fetch call for widgets and rendered them

--- app/javascript/react/components/Dashboard.js
+++ app/javascript/react/components/Dashboard.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import WidgetTile from './WidgetTile'
 
 const Dashboard = () => {
+  const [widgets, setWidgets] = useState([])
+
+  useEffect(() => {
+    fetch('/api/v1/widgets')
+    .then(response => {
+      if (response.ok) {
+        return response.json()
+      } else {
+        throw new Error(`${response.status} ${response.statusText}`)
+      }
+    })
+    .then(parsedBody => {
+      setWidgets(parsedBody)
+    })
+    .catch(error => `Error in fetch ${error.message}`)
+  }, [])
+
+  const widgetTiles = widgets.map(widget => {
+    return(
+      <WidgetTile
+        key={widget.id}
+        type={widget.modulable_type}
+        mod={widget.module}
+      />
+    )
+  })
 
   return (
-    <h1>Dashboard</h1>
+    <h1>{widgetTiles}</h1>
   )
 }
 
