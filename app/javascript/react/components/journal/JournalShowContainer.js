fbcd4a12e3ffc09ee7700275de7ee3e9fb129be3
Crxssroad <gaby_encar28@hotmail.com>
2020-02-13 23:01:31 -0500

    Added missing Redirect import

--- app/javascript/react/components/journal/JournalShowContainer.js
+++ app/javascript/react/components/journal/JournalShowContainer.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, Fragment } from 'react';
+import { Redirect } from 'react-router-dom'
 
 import JournalNewForm from './JournalNewForm';
 import JournalDetails from './JournalDetails';
