25d60db3b8fb0b41321faafff1ad616bfd4a5c90
Crxssroad <gaby_encar28@hotmail.com>
2020-02-15 17:42:04 -0500

    Created weather model

--- app/models/weather.rb
+++ app/models/weather.rb
@@ -3,4 +3,6 @@ class Weather < ApplicationRecord
 
   validates :longitude, presence: true
   validates :latitude, presence: true
+  validates :data, presence: true
+  validates :location, presence: true
 end
