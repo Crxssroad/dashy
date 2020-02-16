7d16daee882a588ba9ef2d1aa5f2ca672f1ce690
Crxssroad <gaby_encar28@hotmail.com>
2020-02-16 12:55:50 -0500

    Made modules optional for widgets that don't require data from the databse

--- app/models/widget.rb
+++ app/models/widget.rb
@@ -1,6 +1,7 @@
 class Widget < ApplicationRecord
-  belongs_to :modulable, :polymorphic => true
+  belongs_to :modulable, :polymorphic => true, optional: true
   belongs_to :user
 
+  validates :modulable_type, inclusion: { in: ['Weather', 'Journal'], message: "Please choose a widget type!" }
   validates :position, presence: true, numericality: { only_integer: true }
 end
