b39cf5e20e4e21c6f7545031a926815df05b6311
Crxssroad <gaby_encar28@hotmail.com>
2020-02-16 21:25:12 -0500

    Created rss feeds table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_16_183235) do
+ActiveRecord::Schema.define(version: 2020_02_17_021752) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,13 @@ ActiveRecord::Schema.define(version: 2020_02_16_183235) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_journals_on_user_id"
+  end
+
+  create_table "rss_feeds", force: :cascade do |t|
+    t.string "url", null: false
+    t.json "data"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
