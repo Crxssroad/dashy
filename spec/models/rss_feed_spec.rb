7386e519899c36f944e1dffff320d573fe5640c4
Crxssroad <gaby_encar28@hotmail.com>
2020-02-17 12:42:16 -0500

    Fixed test names

--- spec/models/rss_feed_spec.rb
+++ spec/models/rss_feed_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Weather, type: :model do
+RSpec.describe RssFeed, type: :model do
   it { should have_valid(:url).when("www.website.com/rss") }
   it { should_not have_valid(:url).when(nil, "") }
 end
--- spec/models/weather_setting_spec.rb
+++ spec/models/weather_setting_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Weather, type: :model do
+RSpec.describe WeatherSetting, type: :model do
   it { should have_valid(:custom).when(true, false) }
-  it { should_not have_valid(:custom).when(nil, "", "some text", 1) }
+  it { should_not have_valid(:custom).when(nil) }
 end
