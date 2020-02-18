c25e38a72cdb9ffad4feb4c5635ce2febc35604c
Crxssroad <gaby_encar28@hotmail.com>
2020-02-18 17:38:21 -0500

    Installed ReactTimeAgo and dependencies, created,

--- app/javascript/react/components/App.js
+++ app/javascript/react/components/App.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
+import JavascriptTimeAgo from 'javascript-time-ago'
+import en from 'javascript-time-ago/locale/en'
+JavascriptTimeAgo.locale(en)
 
 import Topbar from './Topbar'
 
