51604f6506e4215009eaf94c22cc6ad164e7d6c9
Crxssroad <gaby_encar28@hotmail.com>
2020-02-18 21:43:19 -0500

    Refactored StashContainer

--- app/javascript/react/components/stash/StashContainer.js
+++ app/javascript/react/components/stash/StashContainer.js
@@ -1,36 +1,21 @@
 import React, { useState, useEffect, Fragment } from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
-import StashTile from './StashTile';
 import Sidebar from '../Sidebar'
+import StashTile from './StashTile'
 
 const StashContainer = () => {
-  const [journals, setJournals] = useState([])
-
-  useEffect(() => {
-    fetch('/api/v1/stashes')
-    .then(response => {
-      if (response.ok) {
-        return response.json()
-      } else {
-        throw new Error(`${response.status} ${response.statusText}`)
-      }
-    })
-    .then(parsedBody => {
-      setJournals(parsedBody.journals)
-    })
-    .catch(error => console.error(`Error in stash fetch ${error.message}`))
-  }, [])
 
   return(
     <Fragment>
       <Sidebar rootPath="/stash" />
-        <ul>
-          <StashTile
-            type="journals"
-            content={journals}
-          />
-        </ul>
+      <div className="stash-container">
+        <StashTile
+          stashName="journals"
+          parentClass="journal-stash"
+          cardFront={<i className="fas fa-book"></i>}
+          cardBack="Journals"
+        />
+      </div>
     </Fragment>
   )
 }
