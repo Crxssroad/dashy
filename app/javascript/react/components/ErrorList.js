2304a5e89a4dcd547c4c7c08df21ce8541ac9d93
Crxssroad <gaby_encar28@hotmail.com>
2020-02-19 15:33:18 -0500

    Styling changes

--- app/javascript/react/components/ErrorList.js
+++ app/javascript/react/components/ErrorList.js
@@ -3,12 +3,12 @@ import React from 'react'
 const ErrorList = ({ errors }) => {
   const allErrors = errors.map((error, index) => {
     return(
-      <li key={index}>{error}</li>
+      <li key={index}><i className="fas fa-times-circle"></i> {error}</li>
     )
   })
 
   return(
-    <ul>
+    <ul className="error-list">
       {allErrors}
     </ul>
   )
