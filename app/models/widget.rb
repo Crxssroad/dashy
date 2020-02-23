86459114178a1be7ffecd9f7bb2ce0c2cf8b520c
Crxssroad <gaby_encar28@hotmail.com>
2020-02-23 14:31:36 -0500

    Created note model and added it to widget available options

--- app/models/widget.rb
+++ app/models/widget.rb
@@ -2,6 +2,6 @@ class Widget < ApplicationRecord
   belongs_to :modulable, :polymorphic => true
   belongs_to :user
 
-  validates :modulable_type, inclusion: { in: ['WeatherSetting', 'Journal', 'RssFeed'], message: "Please choose a widget type!" }
+  validates :modulable_type, inclusion: { in: ['WeatherSetting', 'Journal', 'RssFeed', 'Note'], message: "Please choose a widget type!" }
   validates :position, presence: true, numericality: { only_integer: true }
 end
