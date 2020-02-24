a003f82f71a133484bd30d29c1feafd87deb9a39
Crxssroad <gaby_encar28@hotmail.com>
2020-02-23 22:04:19 -0500

    Added note to dynamic loading pojos

--- app/javascript/react/components/pojos/widget.js
+++ app/javascript/react/components/pojos/widget.js
@@ -3,8 +3,10 @@ import React from 'react'
 import JournalWidget from '../dashboard/widgets/JournalWidget'
 import WeatherWidget from '../dashboard/widgets/WeatherWidget'
 import RSSWidget from '../dashboard/widgets/RSSWidget'
+import NoteWidget from '../dashboard/widgets/NoteWidget'
 
 import WeatherExpandedWidget from '../dashboard/widgets/WeatherExpandedWidget'
+import NoteExpandedWidget from '../dashboard/widgets/NoteExpandedWidget'
 
 class Widget {
   static load(type, content, parentIndex, setExpandedWidget) {
@@ -17,11 +19,17 @@ class Widget {
     if (type === "rss_feed") {
       return <RSSWidget settings={content} parentIndex={parentIndex} />
     }
+    if (type === "note") {
+      return <NoteWidget content={content} setExpandedWidget={setExpandedWidget} />
+    }
   }
 
   static expand(widget, closeModal) {
     if (widget.type === "Weather") {
       return <WeatherExpandedWidget content={widget.content} closeModal={closeModal} />
+    }
+    if (widget.type === "Note") {
+      return <NoteExpandedWidget content={widget.content} closeModal={closeModal} />
     }
   }
 }
