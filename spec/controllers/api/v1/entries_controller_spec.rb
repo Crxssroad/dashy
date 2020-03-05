2c164caa80e77920acd8317360ea99deea25cf49
Crxssroad <gaby_encar28@hotmail.com>
2020-03-05 10:17:27 -0500

    Added succesful patch test

--- spec/controllers/api/v1/entries_controller_spec.rb
+++ spec/controllers/api/v1/entries_controller_spec.rb
@@ -8,6 +8,12 @@ RSpec.describe Api::V1::EntriesController, type: :controller do
       user: user
     ) }
 
+  let!(:entry) { Entry.create(
+      title: "An unexpected visitor",
+      body: "No one expected to see blue that day...",
+      journal: journal
+    )}
+
   describe "POST#create" do
     let!(:good_post) {
       {
@@ -89,8 +95,28 @@ RSpec.describe Api::V1::EntriesController, type: :controller do
   end
 
   describe "PATCH#update" do
-    context "succesful patch" do
 
+    context "succesful patch" do
+      let!(:good_patch) {
+        {
+          entry: {
+            title: "Mikaelhausen goes on vacation",
+            body: "It all started a couple of weeks ago when, Mikaelhausen, leader of the red pygmies...",
+          },
+          journal_id: journal.id,
+          id: entry.id
+        }
+      }
+      it "should return the newly updated entry" do
+        patch :update, params: good_patch
+        returned_json = JSON.parse(response.body)
+
+        expect(response.status).to eq(200)
+        expect(response.content_type).to eq("application/json")
+
+        expect(returned_json["title"]).to eq(good_patch[:entry][:title])
+        expect(returned_json["body"]).to eq(good_patch[:entry][:body])
+      end
     end
 
     context "unsuccesful patch" do
