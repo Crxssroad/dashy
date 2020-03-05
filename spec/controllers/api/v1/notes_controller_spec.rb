c54c5a1577d83e090c6684efd4a352af5cdec608
Crxssroad <gaby_encar28@hotmail.com>
2020-03-05 13:07:07 -0500

    Added controller test for destroy

--- spec/controllers/api/v1/notes_controller_spec.rb
+++ spec/controllers/api/v1/notes_controller_spec.rb
@@ -92,5 +92,34 @@ RSpec.describe Api::V1::NotesController, type: :controller do
   end
 
   describe "DELETE#destroy" do
+    let!(:good_delete) {
+      {
+        id: note.id
+      }
+    }
+
+    let!(:note_widget) { Widget.create(
+        user: user,
+        modulable_type: note.class.name,
+        modulable_id: note.id,
+        position: 1
+      )}
+    context "succesful delete" do
+      it "should return the deleted note" do
+        delete :destroy, params: good_delete
+        returned_json = JSON.parse(response.body)
+
+        expect(response.status).to eq(200)
+        expect(response.content_type).to eq("application/json")
+
+        expect(returned_json["body"]).to eq(note.body)
+      end
+
+      it "should delete any associated widget records" do
+        widget_id = note_widget.id
+        delete :destroy, params: good_delete
+        expect(Widget.find_by(id: widget_id)).to eq(nil)
+      end
+    end
   end
 end
